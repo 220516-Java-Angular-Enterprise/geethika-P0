b3a66387d5a053de1185ab180eb882045fa27bf9
igeet <105670057+igeet@users.noreply.github.com>
2022-05-30 17:27:33 -0500

    Delete src/java/com/revature/project0 directory

--- src/java/com/revature/project0/ui/AdminMenu.java
+++
@@ -1,8 +0,0 @@
-package com.revature.project0.ui;
-
-public class AdminMenu implements IMenu {
-  
-    @Override
-    public void start(){
-    
-    }
--- src/java/com/revature/project0/ui/IMenu.java
+++
@@ -1,6 +0,0 @@
-package com.revature.project0.ui;
-
-//interface (class with only empty functions that MUST be inherited by children) for all menus to implement
-public interface IMenu {
-    void start();
-}
